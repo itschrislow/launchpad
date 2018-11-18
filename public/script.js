892744a69d70b81ebb46d92cf7d6ba250f78bbe0
Chris Low <chrislow360@gmail.com>
2018-11-18 12:26:32 -0500

    receive imgArr

--- public/script.js
+++ public/script.js
@@ -1,10 +1,18 @@
 // dynamically load images into gallery while has images
-let gallery = document.getElementById('gallery');
-let row = document.createElement('div');
-row.classList.add('row');
-let col = document.createElement('div');
-col.classList.add('col-md-4');
-let card = document.createElement('div');
-card.classList.add('card');
-let img = document.createElement('img');
-img.src = ""; /* get img src from array or database */
+(function() {
+    fetch(`/`)
+        .then(res => res.json())
+        .then(image => console.log(image));
+    // function displayImg(image) {
+        // console.log(image);
+        // let gallery = document.getElementById('gallery');
+        // let row = document.createElement('div');
+        // row.classList.add('row');
+        // let col = document.createElement('div');
+        // col.classList.add('col-md-4');
+        // let card = document.createElement('div');
+        // card.classList.add('card');
+        // let img = document.createElement('img');
+        // img.src = ""; /* get img src from array or database */
+    // }
+}())
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+/**
+ * To allow user to upload and display pictures:
+ * https://medium.com/@deepika.gunda/all-you-need-to-know-about-uploading-and-displaying-pictures-using-node-js-express-js-pug-jade-d89fbeb19947
+ */
+/* Read directory */
 const fs = require('fs');
+/* Get image metadata */
 const ExifImage = require('exif').ExifImage;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/', express.static('public'));
 
 const dir = "./Pics/"
 let imgArr = [];
@@ -20,7 +35,7 @@ imgs.forEach(image => {
                     console.log(newImg);
                     imgArr.push(newImg);
                     // shows array here
-                    console.log(imgArr);
+                    // console.log(imgArr);
                 }
             });
         } catch (err) {
@@ -29,3 +44,16 @@ imgs.forEach(image => {
     }
 });
 // but if i console.log(imgArr) here it prints out an empty array
+
+// TODO: sort array
+
+app.get('/', (req, res) => {
+    res.send(imgArr);
+});
+
+const hostname = 'localhost';
+const port = 4000;
+
+app.listen(port, hostname, () => {
+    console.log('Listening on port 4000');
+});
