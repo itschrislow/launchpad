f508043e60537bdf62c60429cf986dcc335aa756
Chris Low <chrislow360@gmail.com>
2018-11-27 20:33:15 -0500

    display images and add dates dynamically

--- public/script.js
+++ public/script.js
@@ -1,32 +1,70 @@
 /**
  * Dynamically load images into gallery
  */
+let imgArr = [];
+
 (function() {
     fetch(`/pictures`)
-        // .then(data => console.log(data.json))
         .then(res => res.json())
-        // .then(images => console.log(images))
-        .then(images => displayImages(images))
+        .then(images => {
+            imgArr = images;
+            addDates(imgArr);
+            displayImages(images);
+        })
         .catch(err => console.log(err));
-    let container = document.getElementById('container');
-
-    function displayImages(images) {
-        images.forEach(image => displayImage(image));
-    }
-
-    function displayImage(image) {
-        let cardDiv = document.createElement('div');
-        col.classList.add('col-md-4');
-        let anchor = document.createElement('a');
-        anchor.classList.add('lightbox');
-        anchor.href = image.name;
-        let img = document.createElement('img');
-        img.src = image.name;
-        img.alt = "Image not found";
-
-        anchor.appendChild(img);
-        cardContent.appendChild(anchor);
-        cardDiv.appendChild(cardContent);
-        container.appendChild(cardDiv);
-    }
 }())
+
+let container = document.getElementById('container');
+
+function displayAll() {
+    displayImages(imgArr);
+}
+
+function displayImages(images) {
+    container.innerHTML = "";
+    images.forEach(image => displayImage(image));
+}
+
+function displayImage(image) {
+    let cardDiv = document.createElement('div');
+    cardDiv.classList.add('col-md-4');
+    let anchor = document.createElement('a');
+    anchor.classList.add('lightbox');
+    anchor.href = image.name;
+    let img = document.createElement('img');
+    img.src = image.name;
+    img.alt = "Image not found";
+
+    anchor.appendChild(img);
+    cardDiv.appendChild(anchor);
+    container.appendChild(cardDiv);
+}
+
+let dropdownMenu = document.getElementById('dropdown-menu');
+
+function addDates(dates) {
+    let uniqueDate = [];
+    dates.forEach(date => {
+        if(uniqueDate.indexOf(date.time) === -1) {
+            uniqueDate.push(date.time);
+        }
+    })
+    uniqueDate.forEach(date => addDate(date));
+}
+
+function addDate(date) {
+    let btn = document.createElement('button');
+    btn.classList.add('dropdown-item');
+    btn.type = 'button';
+    btn.innerHTML = date;
+    btn.addEventListener("click", function() {
+        filter(date);
+    });
+
+    dropdownMenu.appendChild(btn);
+}
+
+function filter(date) {
+    let filtered = imgArr.filter(img => img.time === date);
+    displayImages(filtered);
+}
--- server.js
+++ server.js
@@ -17,14 +17,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-/* Overrides get '/' endpoint */
 app.use(express.static('public'));
 
 let imgArr = [];
 
 function loadImages(directory) {
     let imgs = fs.readdirSync(directory);
-    console.log(imgs);
+    // console.log(imgs);
 
     imgs.forEach(image => {
         if (image.includes(".JPG")) {
@@ -37,9 +36,9 @@ function loadImages(directory) {
                             name: dir + image,
                             time: exifData.exif.CreateDate.substring(0, 10)
                         }
-                        console.log(newImg);
+                        // console.log(newImg);
                         imgArr.push(newImg);
-                        console.log(imgArr);
+                        // console.log(imgArr);
                     }
                 });
             } catch (err) {
@@ -51,22 +50,15 @@ function loadImages(directory) {
     return imgArr;
 }
 
-function filter() {
-    imgArr.filter(img => {
-        
-    });
+function filter(date) {
+    let filtered = imgArr.filter(img => img.time === date);
+    return filtered;
 }
 
-/* Change endpoint to '/' and remove static serving serve from this endpoint */
 app.get('/pictures', (req, res) => {
     let absDir = "/Users/chris/Desktop/Chris 2018/Projects/Launchpad/public/pics/";
     let arr = loadImages(absDir);
     res.status(200).send(arr);
-});
-
-app.get('/pics/:img', (req, res) => {
-    let image = imgArr.find(img => img.name===req.params.img);
-    res.status(200).send(image);
 });
 
 const hostname = 'localhost';
